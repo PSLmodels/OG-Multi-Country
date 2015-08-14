Correct demographic calculation and adds bool to have unique demographics

Python/7CountryAlphaV2/Main.py
@@ -2,8 +2,8 @@ import numpy as np
 import StepbyStepv1 as Stepfuncs
 
 #Parameters Zone
-I = 2 #Number of countries
-S = 10 #Upper bound of age for agents
+I = 7 #Number of countries
+S = 80 #Upper bound of age for agents
 T = int(round(2.5*S)) #Number of time periods to convergence, based on Rick Evans' function.
 
 T_1 = S #This is like TransYear in the FORTRAN I think
@@ -14,14 +14,16 @@ EndFertilityAge = int(S/80.*45)#The age when agents have their last children
 StartDyingAge = int(S/80.*68)#The first age agents can begin to die
 MaxImmigrantAge = int(S/80.*65)#All immigrants are between ages 0 and MaxImmigrantAge
 g_A = 0.001#Technical growth rate
- 
-beta = .99 #Future consumption discount rate
+
+beta_ann = .95
+beta = beta_ann ** (70/S) #Future consumption discount rate
 sigma = 1 #Leave it at 1, fsolve struggles with the first sigma we used (3).
-delta = .03 #Depreciation Rate
+delta_ann = .08
+delta = 1-(1-delta_ann)**(70/S) #Depreciation Rate
 alpha = .3 #Capital Share of production
 e = np.ones((I, S, T+S+1)) #Labor productivities
 A = np.ones(I) #Techonological Change, used for idential countries
-#A=np.array([1.25,1.35,1,1.65,1.1]) #Techonological Change, used for when countries are different
+#A = np.array([1.25,1.35,1,1.65,1.1]) #Techonological Change, used for when countries are different
 
 diff=1e-12 #Convergence Tolerance
 distance=10 #Used in taking the norm, arbitrarily set to 10
@@ -31,17 +33,20 @@ MaxIters=300 #Maximum number of iterations on TPI.
 #Program Levers
 PrintAges = False #Prints the different key ages in the demographics
 PrintSS = False #Prints the result of the Steady State functions
-CalcTPI = True #Activates the calculation of Time Path Iteration
+CalcTPI = False #Activates the calculation of Time Path Iteration
 #NOTE: Graphing only works if CalcTPI is activated.
 Graphs = True #Activates graphing the graphs.
-CountryNamesON = True #Turns on labels for the graphs. Replaces "Country x" with proper names.
+CountryNamesON = False #Turns on labels for the graphs. Replaces "Country x" with proper names.
+DiffDemog = True #Turns on different demographics over countries. 
 
 #MAIN CODE
 
 #Gets demographic data
-demog_params = (I, S, T, T_1, StartFertilityAge, EndFertilityAge, StartDyingAge, MaxImmigrantAge, g_A, PrintAges)
-FertilityRates, MortalityRates, Migrants, N_matrix, Nhat_matrix = Stepfuncs.getDemographics(demog_params)
-#Stepfuncs.plotDemographics((S,T),0,[0,19],"USA", N_matrix)
+demog_params = (I, S, T, T_1, StartFertilityAge, EndFertilityAge, StartDyingAge, MaxImmigrantAge, g_A)
+FertilityRates, MortalityRates, Migrants, N_matrix, Nhat_matrix = Stepfuncs.getDemographics(demog_params, PrintAges, DiffDemog)
+
+#for i in range(I):
+	#Stepfuncs.plotDemographics((S,T),i,[0,24],str("Country "+str(i)), N_matrix)
 
 #Initalizes initial guesses
 assets_guess = np.ones((I, S-1))*.15

Python/7CountryAlphaV2/StepbyStepv1.py
@@ -1,12 +1,14 @@
 from __future__ import division
+import sys
 import numpy as np
 import scipy as sp
 import scipy.optimize as opt
+import time as time
 from matplotlib import pyplot as plt
 
 #DEMOGRAPHICS FUNCTIONS
 
-def getDemographics(params):
+def getDemographics(params, PrintAges, DiffDemog):
 	"""
 	Description:
 		-Imports data from csv files for initial populations, fertility rates, mortality rates, and net migrants. 
@@ -36,7 +38,17 @@ def getDemographics(params):
 		-Nhat matrix: Numpy array that contains the population percentage for all countries, ages, and years
 	"""
 
-	I, S, T, T_1, StartFertilityAge, EndFertilityAge, StartDyingAge, MaxImmigrantAge, g_A, PrintAges = params
+	I, S, T, T_1, StartFertilityAge, EndFertilityAge, StartDyingAge, MaxImmigrantAge, g_A = params
+
+	#Initializes demographics matrices
+	N_matrix = np.zeros((I, S+1, T+S+1))
+	Nhat_matrix = np.zeros((I, S+1, T+S+1))
+	#N_temp = np.zeros((I, S+1, T+S+1))
+	FertilityRates = np.zeros((I, S+1, T+S+1))
+	MortalityRates = np.zeros((I, S+1, T+S+1))
+	Migrants = np.zeros((I, S+1, T+S+1))
+	g_N = np.zeros(T+S+1)
+
 	if PrintAges:
 		print "T =", T
 		print "T_1", T_1
@@ -45,37 +57,47 @@ def getDemographics(params):
 		print "StartDyingAge", StartDyingAge
 		print "MaxImmigrantAge", MaxImmigrantAge
 
-	#Imports and scales data for the USA. Imports a certain number of generations according to the value of S
-	USAPopdata = np.loadtxt(("Data_Files/population.csv"),delimiter=',',skiprows=1, usecols=[1])[:S+1]*1000
-	USAFertdata = np.loadtxt(("Data_Files/usa_fertility.csv"),delimiter=',',skiprows=1, usecols=range(1,EndFertilityAge+2-StartFertilityAge))[48:48+T_1,:]
-	USAMortdata = np.loadtxt(("Data_Files/usa_mortality.csv"),delimiter=',',skiprows=1, usecols=range(1,S+1-StartDyingAge))[:T_1,:]
-	USAMigdata = np.loadtxt(("Data_Files/net_migration.csv"),delimiter=',',skiprows=1, usecols=[1])[:MaxImmigrantAge]*100
+	if DiffDemog:
+		
+		if I > 7:
+			sys.exit("ERROR!!! We can't have more than 7 Countries without unique data. Change either parameter I so it is less than 8 or change DiffDemog to False")
+		
+		countrynames = ["usa", "eu", "japan", "china", "india", "russia", "korea"]
 
-	#Initializes demographics matrices
-	N_matrix = np.zeros((I, S+1, T))
-	Nhat_matrix = np.zeros((I, S+1, T))
-	FertilityRates = np.zeros((I, S+1, T))
-	MortalityRates = np.zeros((I, S+1, T))
-	Migrants = np.zeros((I, S+1, T))
-	g_N = np.zeros(T)
+		for i in range(I):
+			print countrynames[i]
+			N_matrix[i,:,0] = np.loadtxt(("Data_Files/population.csv"),delimiter=',',skiprows=1, usecols=[i+1])[:S+1]*1000
+			FertilityRates[i,StartFertilityAge:EndFertilityAge+1,:T_1] = np.transpose(np.loadtxt(str("Data_Files/" + countrynames[i] + "_fertility.csv"),delimiter=',',skiprows=1, usecols=range(1,EndFertilityAge+2-StartFertilityAge))[48:48+T_1,:])
+			MortalityRates[i,StartDyingAge:-1,:T_1] = np.transpose(np.loadtxt(str("Data_Files/" + countrynames[i] + "_mortality.csv"),delimiter=',',skiprows=1, usecols=range(1,S+1-StartDyingAge))[:T_1,:])
+			Migrants[i,:MaxImmigrantAge,:T_1] = np.einsum("s,t->st",np.loadtxt(("Data_Files/net_migration.csv"),delimiter=',',skiprows=1, usecols=[i+1])[:MaxImmigrantAge]*100, np.ones(T_1))
 
-	#NOTE: For now we set fertility, mortality, number of migrants, and initial population the same for all countries. 
-	
-	#Sets initial total population (N_matrix), percentage of total world population (Nhat_matrix)
-	N_matrix[:,:,0] = np.tile(USAPopdata, (I, 1))
+	else:
+		#Imports and scales data for the USA. Imports a certain number of generations according to the value of S
+		USAPopdata = np.loadtxt(("Data_Files/population.csv"),delimiter=',',skiprows=1, usecols=[1])[:S+1]*1000
+		USAFertdata = np.loadtxt(("Data_Files/usa_fertility.csv"),delimiter=',',skiprows=1, usecols=range(1,EndFertilityAge+2-StartFertilityAge))[48:48+T_1,:]
+		USAMortdata = np.loadtxt(("Data_Files/usa_mortality.csv"),delimiter=',',skiprows=1, usecols=range(1,S+1-StartDyingAge))[:T_1,:]
+		USAMigdata = np.loadtxt(("Data_Files/net_migration.csv"),delimiter=',',skiprows=1, usecols=[1])[:MaxImmigrantAge]*100
+
+		#NOTE: For now we set fertility, mortality, number of migrants, and initial population the same for all countries. 
+
+		#Sets initial total population
+		N_matrix[:,:,0] = np.tile(USAPopdata, (I, 1))
+
+		#Fertility Will be equal to 0 for all ages that don't bear children
+		FertilityRates[:,StartFertilityAge:EndFertilityAge+1,:T_1] = np.einsum("ts,i->ist", USAFertdata, np.ones(I))
+
+		#Mortality be equal to 0 for all young people who aren't old enough to die
+		MortalityRates[:,StartDyingAge:-1,:T_1] = np.einsum("ts,it->ist", USAMortdata, np.ones((I,T_1)))
+
+		#The number of migrants is the same for each year
+		Migrants[:,:MaxImmigrantAge,:T_1] = np.einsum("s,it->ist", USAMigdata, np.ones((I,T_1)))
+
+
 	Nhat_matrix[:,:,0] = N_matrix[:,:,0]/np.sum(N_matrix[:,:,0])
-
-	#Fertility Will be equal to 0 for all ages that don't bear children
-	FertilityRates[:,StartFertilityAge:EndFertilityAge+1,:T_1] = np.einsum("ts,i->ist", USAFertdata, np.ones(I))
-
-	#Mortality be equal to 0 for all young people who aren't old enough to die
-	MortalityRates[:,StartDyingAge:-1,:T_1] = np.einsum("ts,it->ist", USAMortdata, np.ones((I,T_1)))
+	N_temp = np.ones((I, S+1))/(I*S)
 
 	#The last generation dies with probability 1
-	MortalityRates[:,-1,:] = np.ones((I, T))
-
-	#The number of migrants is the same for each year
-	Migrants[:,:MaxImmigrantAge,:T_1] = np.einsum("s,it->ist", USAMigdata, np.ones((I,T_1)))
+	MortalityRates[:,-1,:] = np.ones((I, T+S+1))
 
 	#Gets steady-state values
 	f_bar = FertilityRates[:,:,T_1-1]
@@ -83,34 +105,46 @@ def getDemographics(params):
 	m_bar = Migrants[:,:,T_1-1]
 
 	#Set to the steady state for every year beyond year T_1
-	FertilityRates[:,:,T_1:] = np.tile(np.expand_dims(f_bar, axis=2), (1,1,T-T_1))
-	MortalityRates[:,:,T_1:] = np.tile(np.expand_dims(p_bar, axis=2), (1,1,T-T_1))
-	Migrants[:,:,T_1:] = np.tile(np.expand_dims(m_bar, axis=2), (1,1,T-T_1))
+	FertilityRates[:,:,T_1:] = np.tile(np.expand_dims(f_bar, axis=2), (1,1,T-T_1+S+1))
+	MortalityRates[:,:,T_1:] = np.tile(np.expand_dims(p_bar, axis=2), (1,1,T-T_1+S+1))
+	Migrants[:,:,T_1:] = np.tile(np.expand_dims(m_bar, axis=2), (1,1,T-T_1+S+1))
 
 	#Gets the initial immigration rate
 	ImmigrationRate = Migrants[:,:,0]/N_matrix[:,:,0]
 
 	#Gets initial world population growth rate
-	g_N[0] = np.sum(Nhat_matrix[:,:,0]*(FertilityRates[:,:,0] + ImmigrationRate - MortalityRates[:,:,0]))
+	g_N[0] = 0.
 
 	#Calculates population numbers for each country
-	for t in range(1,T):
+	for t in range(1,T+S+1):
 		#Gets the total number of children and and percentage of children and stores them in generation 0 of their respective matrices
 		#See equations 2.1 and 2.10
 		N_matrix[:,0,t] = np.sum((N_matrix[:,:,t-1]*FertilityRates[:,:,t-1]),axis=1)
-		Nhat_matrix[:,0,t] = np.exp(-g_N[t-1])*np.sum((Nhat_matrix[:,:,t-1]*FertilityRates[:,:,t-1]),axis=1)
+		N_temp[:,0] = np.sum((Nhat_matrix[:,:,t-1]*FertilityRates[:,:,t-1]),axis=1)
 
 		#Finds the immigration rate for each year
 		ImmigrationRate = Migrants[:,:,t-1]/N_matrix[:,:,t-1]
 
-		#Gets the population and percentage of population for the next year, taking into account immigration and mortality
+		#Gets the population distribution and percentage of population distribution for the next year, taking into account immigration and mortality
 		#See equations 2.2 and 2.11
 		N_matrix[:,1:,t] = N_matrix[:,:-1,t-1]*(1+ImmigrationRate[:,:-1]-MortalityRates[:,:-1,t-1])
-		Nhat_matrix[:,1:,t] = np.exp(-g_N[t-1])*Nhat_matrix[:,:-1,t-1]*(1+ImmigrationRate[:,:-1]-MortalityRates[:,:-1,t-1])
-		
+		Nhat_matrix[:,:,t] = N_matrix[:,:,t]/np.sum(N_matrix[:,:,t])
+		N_temp[:,1:] = Nhat_matrix[:,:-1,t-1]*(1+ImmigrationRate[:,:-1]-MortalityRates[:,:-1,t-1])
+
+
 		#Gets the growth rate for the next year
-		g_N[t] = np.sum(Nhat_matrix[:,:,t]*(FertilityRates[:,:,t] + ImmigrationRate - MortalityRates[:,:,t]), axis=(0, 1))
-		#print np.sum(Nhat_matrix[:,:,t])#This should be equal to 1.0
+		g_N[t] = np.sum(N_temp[:,:])-1
+
+	"""
+	plt.plot(range(T+S+1), np.sum(Nhat_matrix, axis=(0,1)), label="Nhat")
+	#plt.plot(range(T+S+1), np.sum(N_temp, axis=(0,1)), label="N_temp")
+	plt.plot(range(T+S+1), np.exp(g_N), label="g_N")
+	plt.legend(loc=1)
+	ax = plt.gca()
+	ax.grid(True)
+	plt.show()
+	plt.clf()
+	"""
 
 	#Gets labor endowment per household. For now it grows at a constant rate g_A
 	l_endowment = np.cumsum(np.ones(T)*g_A)