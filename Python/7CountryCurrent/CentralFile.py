9031bc4f60ea5bb9bf349629ad128295856ea021
James <olmstjam@byu.edu>
2015-10-30 01:09:32 -0600

    still cleaning

--- Python/7CountryCurrent/CentralFile.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import scipy as sp
-import Functions as func
-import Demographics as demog
-import Optimize as op
-
-'''
-
-'''
--- Python/7CountryCurrent/Optimize.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import scipy as sp
-import Functions as func
-import Demographics as demog
-
-'''
-'''
